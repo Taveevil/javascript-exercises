58f68275ce9084722739238007cc16832de70d55
Tavee Villamar <hello@tavee.ca>
2023-06-10 18:57:35 -0500

    solve tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  let total  = (value - 32) * 5/9
+  total = Math.round(total * 10)/10;
+  return total;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  let total = (value * 9/5) + 32;
+  total = Math.round(total * 10)/10;
+  return total;
 };
 
 // Do not edit below this line
